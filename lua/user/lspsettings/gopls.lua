c89370940694378432213a0b8619589cff3c930b
sebamzz <sebastianmazzeo25@gmail.com>
2024-06-12 22:11:37 -0300

    add custom gopls, inlay hint need this for work

---
+++ lua/user/lspsettings/gopls.lua
@@ -0,0 +1,44 @@
+return {
+  settings = {
+    gopls = {
+      analyses = {
+        unreachable = true,
+        nilness = true,
+        unusedparams = true,
+        useany = true,
+        unusedwrite = true,
+        ST1003 = true,
+        undeclaredname = true,
+        fillreturns = true,
+        nonewvars = true,
+        fieldalignment = false,
+        shadow = true,
+      },
+      codelenses = {
+        generate = true, -- show the `go generate` lens.
+        gc_details = true, -- Show a code lens toggling the display of gc's choices.
+        test = true,
+        tidy = true,
+        vendor = true,
+        regenerate_cgo = true,
+        upgrade_dependency = true,
+      },
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+      usePlaceholders = true,
+      completeUnimported = true,
+      staticcheck = true,
+      matcher = "Fuzzy",
+      diagnosticsDelay = "500ms",
+      symbolMatcher = "fuzzy",
+      semanticTokens = true,
+    },
+  },
+}
